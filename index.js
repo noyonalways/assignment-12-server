aacaa6662987e20ceb19bef11fb4c9b1d5a2de4d
codernoyon <noyonrahman2003@gmail.com>
2022-05-24 03:51:46 +0600

    home banner section slider api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,6 +12,28 @@ const app = express();
 app.use(cors());
 app.use(express());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjons.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const bannerSlideCollection = client.db('techParts').collection('bannerSlideCollection');
+
+        app.get('/home-slider', async(req,res) => {
+            const homeSliders = await bannerSlideCollection.find().toArray();
+            res.send({success: true, data: homeSliders});
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 // testing server
 app.get('/', (req, res) => {
