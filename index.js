7fac6549bb189a7b1eb3145499d80979874c63b4
codernoyon <noyonrahman2003@gmail.com>
2022-05-25 12:53:05 +0600

    product upadate api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -22,6 +22,8 @@ async function run() {
         await client.connect();
         const bannerSlideCollection = client.db('techParts').collection('bannerSlideCollection');
         const questionCollection = client.db('techParts').collection('questionCollection');
+        const reviewCollection = client.db('techParts').collection('reviewCollection');
+        const productCollection = client.db('techParts').collection('productCollection');
 
         // get the home banner silder data
         app.get('/home-slider', async (req, res) => {
@@ -33,7 +35,45 @@ async function run() {
         app.get('/question', async (req, res) => {
             const questions = await questionCollection.find().toArray();
             res.send({ success: true, data: questions });
-        })
+        });
+
+        // get all reviews
+        app.get('/review', async(req, res) => {
+            const reviews = await reviewCollection.find().toArray();
+            res.send({success: true, data: reviews});
+        });
+
+        // get all products
+        app.get('/product', async(req, res) => {
+            const products = await productCollection.find().toArray();
+            res.send({success: true, data: products});
+        });
+
+        // get single product
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        // update  product 
+        app.put('/product/:id', async(req, res) => {
+            const id = req.params.id;
+            const getProduct = req.body;
+            const updatedQuantity = getProduct.quantity;
+            const updatedSold = getProduct.sold;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updatedProduct = {
+                $set: {
+                    availableQuantity: updatedQuantity,
+                    sold : updatedSold
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedProduct, options);
+            res.send({success: true, message: 'Updated Succesfully', result});
+        });
 
     }
     finally {
