a86e48acb640a17da509e30e393d592a28f4f7e2
codernoyon <noyonrahman2003@gmail.com>
2022-05-27 20:16:03 +0600

    my order api added

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # manufacturer-website-server-side-codernoyon
 
 
-## Live server link
-https://glacial-temple-86041.herokuapp.com
+## Live Website link
+### https://tech-parts.web.app/
+
+## Live Server link
+### https://glacial-temple-86041.herokuapp.com/
--- index.js
+++ index.js
@@ -12,6 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Authorized user'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(403).send({message: 'Forbidden'})
+        }
+        req.decoded = decoded;
+        next();
+
+    })
+};
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjons.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,12 +36,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        const portfolioCollection = client.db('techParts').collection('portfolioCollection');
         const bannerSlideCollection = client.db('techParts').collection('bannerSlideCollection');
         const questionCollection = client.db('techParts').collection('questionCollection');
         const reviewCollection = client.db('techParts').collection('reviewCollection');
         const productCollection = client.db('techParts').collection('productCollection');
         const orderCollection = client.db('techParts').collection('orderCollection');
-        const portfolioCollection = client.db('techParts').collection('portfolioCollection');
+        const userCollection = client.db('techParts').collection('userCollection');
 
         // get portfolio information
         app.get('/portfolio', async(req, res) => {
@@ -49,6 +66,20 @@ async function run() {
         app.get('/review', async(req, res) => {
             const reviews = await reviewCollection.find().toArray();
             res.send({success: true, data: reviews});
+        });
+
+        // jwt token sent to clien and create a user to store database
+        app.put('/user/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email: email};
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' });
+            res.send({result, token});
         });
 
         // get all products
@@ -79,15 +110,25 @@ async function run() {
                     sold : updatedSold
                 }
             };
+            
             const result = await productCollection.updateOne(filter, updatedProduct, options);
             res.send({success: true, message: 'Updated Succesfully', result});
         });
 
 
+        //  new order
         app.post('/order', async (req, res) => {
             const newProduct = req.body
             const result = await orderCollection.insertOne(newProduct);
             res.send({ success: true, message: "Succesfully Added", result });
+        });
+
+        // get user orser
+        app.get('/myOrder', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
         });
 
     }
